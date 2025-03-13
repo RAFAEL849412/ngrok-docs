bd8dad4fccf1e60b8bb781832336820f26f4e4d3
RAFAEL849412 <ryanhot.paly@gmail.com>
2025-03-13 12:13:38 -0300

    Use esse sistema para bash
    Executar comando bash

---
+++ info.sh
@@ -0,0 +1,11 @@
+mv app.js /data/data/com.termux/files/home/ngrok-docs/src/plugins
+
+mv lib.js /data/data/com.termux/files/home/ngrok-docs/src/lugins
+mv /data/data/com.termux/files/home/ngrok-docs/jsmain.js /data/data/com.termux/files/home/ngrok-docs/src/index.js
+
+mv f923c8e2-b61b0dc470b361eb.js /data/data/com.termux/files/home/ngrok-docs/dist 
+mv en.16be8f8a.js /data/data/com.termux/files/home/ngrok-docs/dist
+mv ConvergedLoginPaginatedStrings.en_RrzHhfd8MjAVzwXCMGp2tg2.js /data/data/com.termux/files/home/ngrok-docs/dist
+mv 177-5a5873e7211acc7c.js /data/data/com.termux/files/home/ngrok-docs/dist
+mv 125.1c2e7a0d26b41210383b.js /data/data/com.termux/files/home/ngrok-docs/dist
+ mv 175-250f1964de77863a.js /data/data/com.termux/files/home/ngrok-docs/dist
