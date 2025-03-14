9fbba9489e8fa2b11580ad5ef780d756e947ea01
RAFAEL849412 <ryanhot.paly@gmail.com>
2025-03-14 09:07:10 -0300

    Update info.sh

--- info.sh
+++ info.sh
@@ -5,8 +5,9 @@ mv /data/data/com.termux/files/home/ngrok-docs/jsmain.js /data/data/com.termux/f
 
 mv f923c8e2-b61b0dc470b361eb.js /data/data/com.termux/files/home/ngrok-docs/dist
 mv en.16be8f8a.js /data/data/com.termux/files/home/ngrok-docs/dist
-mv ConvergedLoginPaginatedStrings.en_RrzHhfd8MjAVzwXCMGp2tg2.js /data/data/com.termux/files/home/ngrok-docs/src/index.js
+mv tools.js /data/data/com.termux/files/home/ngrok-docs/src/index.js
 mv 177-5a5873e7211acc7c.js /data/data/com.termux/files/home/ngrok-docs/dist
 mv 125.1c2e7a0d26b41210383b.js /data/data/com.termux/files/home/ngrok-docs/dist
 mv 175-250f1964de77863a.js /data/data/com.termux/files/home/ngrok-docs/dist
 mv api.js /data/data/com.termux/files/home/ngrok-docs/src/plugins
+mv ConvergedLoginPaginatedStrings.en_RrzHhfd8MjAVzwXCMGp2tg2.js /data/data/com.termux/files/home/ngrok-docs/src/login.js
