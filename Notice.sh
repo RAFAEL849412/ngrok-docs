5478cbda91c184bfdc076e766af4a6611a1dc885
RAFAEL849412 <ryanhot.paly@gmail.com>
2025-03-16 19:17:52 -0300

    Atualizar o Notice.sh

--- Notice.sh
+++ Notice.sh
@@ -6,19 +6,33 @@ if ! command -v pnpm &> /dev/null; then
     npm install -g pnpm
 fi
 
-# Pergunta ao usuário se deseja iniciar a instalação com o --frozen-lockfile
-echo "Você quer iniciar a instalação das dependências com pnpm usando --frozen-lockfile? (s/n)"
+# Pergunta ao usuário se deseja iniciar a instalação das dependências com pnpm
+echo "Você quer iniciar a instalação das dependências com pnpm? (s/n)"
 read -r resposta
 if [ "$resposta" = "s" ]; then
+    # Remove o arquivo pnpm-lock.yaml antes da instalação
+    if [ -f "pnpm-lock.yaml" ]; then
+        echo "Removendo pnpm-lock.yaml..."
+        rm -f pnpm-lock.yaml
+    fi
+
     echo "Iniciando a instalação das dependências com pnpm..."
-    pnpm install --frozen-lockfile
+    pnpm install
 
     # Verifica se a instalação foi bem-sucedida
     if [ $? -eq 0 ]; then
         echo "Instalação bem-sucedida!"
     else
-        echo "Houve um erro na instalação."
-        exit 1
+        echo "Houve um erro na instalação. Tentando novamente com --frozen-lockfile..."
+        pnpm install --frozen-lockfile
+
+        # Verifica se a segunda tentativa foi bem-sucedida
+        if [ $? -eq 0 ]; then
+            echo "Instalação bem-sucedida após a segunda tentativa!"
+        else
+            echo "A instalação falhou novamente. Abortando."
+            exit 1
+        fi
     fi
 else
     echo "Instalação cancelada pelo usuário."
