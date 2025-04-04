1f6eb4f0e7bd685c4a3eaef38f8996c353a19746
RAFAEL849412 <ryanhot.paly@gmail.com>
2025-04-04 18:09:10 -0300

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Cria as pastas necessárias
-mkdir -p .github/.workflows
+# Cria a pasta necessária
+mkdir -p .github/workflows
 
-# Move os arquivos para a pasta .github/.workflows se eles existirem
-for file in .autofix.yml .ci-checks.yml .deploy.yml .webpack.yml .write-rss.yml; do
+# Lista de arquivos a serem movidos
+files=(.autofix.yml .ci-checks.yml .deploy.yml .webpack.yml .write-rss.yml docker-compose.yaml)
+
+# Move os arquivos para a pasta .github/workflows se eles existirem
+for file in "${files[@]}"; do
     if [ -e "$file" ]; then
-        mv "$file" .github/.workflows/
+        mv "$file" .github/workflows/
     else
         echo "Arquivo $file não encontrado, pulando..."
     fi
